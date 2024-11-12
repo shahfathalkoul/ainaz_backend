2eb735dea5097ed2fb50a995a2c81c2d96b0afc4
shah fathal koul <insanefthl@FTHLs-MacBook-Pro.local>
2024-11-12 19:18:23 +0530

    final

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const defaultdb = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.PORT
 });
 
 defaultdb.connect((err) => {
